dd0d3bec9333a301de33f2d7d016ccb6ef064896
webwinnertechsolution <webwinnertechsolution@gmail.com>
2026-05-14 17:00:29 +0530

    Add API and uploads proxy for live

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,25 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['127.0.0.1'],
+    domains: ['127.0.0.1', 'sainivetpharma-api.web-winners.com'],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60 * 60 * 24 * 365,
   },
   async rewrites() {
-    return [
-      {
-        source: '/uploads/:path*',
-        destination: 'http://127.0.0.1:8000/uploads/:path*',
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: '/api/:path*',
+          destination: 'https://sainivetpharma-api.web-winners.com/api/:path*',
+        },
+        {
+          source: '/uploads/:path*',
+          destination: 'https://sainivetpharma-api.web-winners.com/uploads/:path*',
+        },
+      ],
+    };
   },
 };
 export default nextConfig;
