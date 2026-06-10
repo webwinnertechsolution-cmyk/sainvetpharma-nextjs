beac9fce478b53d34e80f564dfed752dd23bbf37
webwinnertechsolution-cmyk <webwinnertechsolution@gmail.com>
2026-06-10 16:22:53 +0530

    Update Slider.jsx

--- app/components/home/Slider.jsx
+++ app/components/home/Slider.jsx
@@ -89,7 +89,7 @@ export default function Slider({ sliders }) {
           font-family: 'Nunito', sans-serif;
         }
         .rl-slide-title {
-          color: #1a2e44;
+          color: #ffffff;
           font-size: 52px;
           font-weight: 800;
           line-height: 1.15;
